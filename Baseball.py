18c0a14bb95c3df95d527382b5f879d9c6eb1b2f
Robert <rmick@mickx009.org>
2025-07-08 18:17:10 -0700

    Added list of that day's games.

--- Baseball.py
+++ Baseball.py
@@ -2,38 +2,27 @@ import requests
 from pprint import pprint
 import json
 
-#date = input("Today's Date: ")
+
+blanklist = []
 url = "http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1"
 response = requests.get(url)
 MLB = response.json()
 date = MLB['dates'][0]['date']
+print("Today's MLB Schedule!")
 print(date)
+games = MLB['dates'][0]['games'][0:]
+for x in games:
+    blanklist.append(x['teams'])
 
-awaylist = []
-homelist = []
+away = []
+home = []
+for ateam in blanklist:
+    away.append(ateam['away']['team']['name'])
 
-dataaway = MLB['dates'][0]['games'][0]['teams']['away']['team']['name']
-datahome = MLB['dates'][0]['games'][0]['teams']['home']['team']['name']
+for hteam in blanklist:
+    home.append(hteam['home']['team']['name'])
 
-for team1 in dataaway:
-    awaylist.append(team1)
-
-for team2 in datahome:
-    homelist.append(team2)
-
-awayteams = ' '.join(awaylist)
-hometeams = ' '.join(homelist)
-
-finalaway = awayteams
-finalhome = hometeams
-
-print(finalaway +  " AT "  + finalhome)
-#print(homelist)
-
-
-
-# print(dataaway)
-# print("vs.")
-# print(datahome)
-
+for a, h in zip(away, home):
+    print("-----")
+    print(a + " At " + h)
 
