279b95cc61a1eb11a7ba546fc0d8d59b5a3bbeb2
Robert <rmick@mickx009.org>
2025-07-17 07:16:02 -0700

    nc

--- Sports/BaseballAPI/fetchingbaseball_NoScore.py
+++ Sports/BaseballAPI/fetchingbaseball_NoScore.py
@@ -1,6 +1,7 @@
 import requests
 from pprint import pprint
 import json
+from tkinter import *
 
 
 datalist = []
@@ -34,3 +35,24 @@ for final in games:
 for a, h, f in zip(away, home, status):
     print("-----")
     print(a + " " + " At " + h + " " + " " + "----" + " " + f)
+
+for a, h, f in zip(away, home, status):
+    with open("todaysgames.txt", "w") as file:
+        file.write(a + " " + " At " + h + " " + " " + "----" + " " + f + "\n")
+
+#Create Tkinter Window and print from TXT file
+root = Tk()
+root.title("Today's MLB Games")
+root.geometry("800x650")
+#root.configure(bg="green")
+
+schedule = open("todaysgames.txt", "r")
+content = schedule.read()       
+
+my_text = Text(root, width=100, height=100, font=("Helvetica", 20))
+my_text.pack(expand=True, fill="both",pady=60)
+my_text.tag_configure("center", justify='center')
+my_text.insert(END, content)
+my_text.tag_add("center", "1.0", "end")
+
+root.mainloop()
